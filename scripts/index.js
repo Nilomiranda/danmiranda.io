c814a4e0c05a3443973b789e04561803de97fe88
Danilo Miranda <nilomiranda3@gmail.com>
2018-10-14 16:05:34 -0300

    Added js functionality to cut the post preview text if too long

--- scripts/index.js
+++ scripts/index.js
@@ -7,3 +7,23 @@ const showMenu = () => {
 }
 
 menuButton.addEventListener('click', showMenu);
+
+/**
+ * This will change the post preview text in case it's too long
+ * The limit is 68 characters. If the preview text is longer than that
+ * the text will be cut to 65 characters and then a " ..." will be added
+ * at the end.
+ */
+
+const postPreviews = Array.from(document.querySelectorAll('.post-preview'));
+
+const newPostPreview = postPreviews.map(postPreview => {
+  if(postPreview.innerText.length > 68) {
+    const bigText = postPreview.innerText;
+    const newText = bigText.slice(0, 65).concat(' ...');
+    postPreview.innerText = newText;
+    return newText;
+  } else {
+    return postPreview.innerText;
+  }
+});
